b513782706944f946c647bfbbf94cea3faed96fa
vinh-nd2002 <nguduyvinh2k2@gmail.com>
2022-04-27 21:54:46 +0700

    set up

---
+++ booking-movie-ticket/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container text-center text-5xl bg-teal-400">
+      <h1>Hello anh vinh</h1>
+    </div>
+  );
+}
+
+export default App;
